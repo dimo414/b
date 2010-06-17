3d45e6c9895fe7ee554cd72dbb45408639c0987b
Michael Diamond@Artemis <dimo414@gmail.com>
2010-06-16 19:34:00 -0700

    Details, minor fixes

--- src/bugs.py
+++ src/bugs.py
@@ -10,7 +10,8 @@ version = "Bugs 0.1.0"
 #
 # Imports
 #
-import os, re, sys, hashlib, random
+import os, errno, re, hashlib, sys
+from datetime import datetime
 
 #
 # Exceptions
@@ -51,6 +52,10 @@ class UnknownPrefix(Exception):
 #       
 # Helper Methods - often straight from t
 #
+def _timestamp():
+        now = datetime.now()
+        return now.strftime("%A, %B %d %Y %I:%M%p")
+
 def _hash(text):
     """Return a hash of the given text for use as an id.
     
@@ -58,6 +63,17 @@ def _hash(text):
     
     """
     return hashlib.sha1(text.encode('utf-8')).hexdigest()
+
+def _mkdir_p(path):
+    """ race condition handling recursive mkdir call
+    http://stackoverflow.com/questions/600268/mkdir-p-functionality-in-python
+    """
+    try:
+        os.makedirs(path)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST:
+            pass
+        else: raise
 
 def _task_from_taskline(taskline):
     """Parse a taskline (from a task file) and return a task.
@@ -141,6 +157,15 @@ def _prefixes(ids):
         del pre[':']
     return pre
 
+def _describe_print(num,type,owner,filter):
+    typeName = 'open' if type else 'resolved'
+    out = "Found %s %s bug%s" % (num, typeName, '' if num==1 else 's')
+    if owner != '*':
+        out = out+(" owned by %s" % 'Nobody' if owner=='' else owner)
+    if filter != '':
+        out = out+" whose title contains %s" % filter
+    return out
+
 #
 # Primary Class
 #
@@ -154,11 +179,20 @@ class BugsDict(object):
     and therefore anything calling this class ought to handle that change
     (normally to the repo root)
     """
-    def __init__(self, bugsdir='.bugs'):
+    def __init__(self, autodetails=True,bugsdir='.bugs'):
         """Initialize by reading the task files, if they exist."""
+        self.autodetail = autodetails
         self.file = 'bugs'
         self.bugsdir = bugsdir
+        self.detailsdir = 'details'
         self.bugs = {}
+        self.init_details = ("Details for '%s'\nID: %s\nFiled: %s\n\n"
+        "[paths]\n# Paths related to this bug.\n# suggested format: REPO_PATH:LINENUMBERS\n\n\n"
+        "[details]\n# Additional details\n\n\n"
+        "[expected]\n# The expected result\n\n\n"
+        "[actual]\n# What happened instead\n\n\n"
+        "[reproduce]\n# Reproduction steps\n\n\n"
+        "[comments]\n# Comments and updates - leave your name\n")
         path = os.path.join(os.path.expanduser(self.bugsdir), self.file)
         if os.path.isdir(path):
             raise InvalidTaskfile
@@ -178,24 +212,39 @@ class BugsDict(object):
         If no tasks match the prefix an UnknownPrefix exception will be raised.
         
         """
-        matched = list(filter(lambda tid: tid.startswith(prefix), self.tasks.keys()))
+        matched = list(filter(lambda tid: tid.startswith(prefix), self.bugs.keys()))
         if len(matched) == 1:
-            return self.tasks[matched[0]]
+            return self.bugs[matched[0]]
         elif len(matched) == 0:
             raise UnknownPrefix(prefix)
         else:
-            matched = list(filter(lambda tid: tid == prefix, self.tasks.keys()))
+            matched = list(filter(lambda tid: tid == prefix, self.bugs.keys()))
             if len(matched) == 1:
-                return self.tasks[matched[0]]
+                return self.bugs[matched[0]]
             else:
                 raise AmbiguousPrefix(prefix)
+            
+    def _make_details_file(self,id):
+        dirpath = os.path.join(self.bugsdir,self.detailsdir)
+        path = os.path.join(dirpath,id+".txt")
+        if not os.path.exists(dirpath):
+            _mkdir_p(dirpath)
+        if os.path.isdir(path):
+            raise InvalidDetailsFile(id)
+        if not os.path.exists(path):
+            f = open(path, "w+")
+            f.write(self.init_details % (self.bugs[id]['text'],self.bugs[id]['id'],_timestamp()))
+            f.close()
+        return path
     
-    def add_bug(self, text):
+    def add(self, text):
         """Adds a bug with no owner to the task list"""
         task_id = _hash(text)
-        self.tasks[task_id] = {'id': task_id, 'open': 'True', 'owner': '', 'text': text}
+        self.bugs[task_id] = {'id': task_id, 'open': 'True', 'owner': '', 'text': text}
+        if self.autodetail:
+            self._make_details_file(task_id)
     
-    def rename_bug(self, prefix, text):
+    def rename(self, prefix, text):
         """Renames the bug
         
         If more than one task matches the prefix an AmbiguousPrefix exception
@@ -211,34 +260,59 @@ class BugsDict(object):
             text = re.sub(find, repl, task['text'])
         
         task['text'] = text
+        
+        path = os.path.join(self.bugsdir,self.detailsdir,self[prefix]['id']+".txt")
+        if os.path.exists(path):
+            print("At present, the details file for this bug shows the original name still.  "
+                  "This is a known bug, but you'll need to edit the details manually.")
     
-    def assign_bug(self, prefix, user,force=False):
+    def assign(self, prefix, user,force=False):
         """Specifies a new owner of the bug.  Warns if the owner doesn't exist"""
         pass
     
-    def details_bug(self, prefix):
-        """Returns the details of a specified bug if they exist"""
-        pass
+    def details(self, prefix):
+        path = os.path.join(self.bugsdir,self.detailsdir,self[prefix]['id']+".txt")
+        if (not os.path.exists(path)):
+            raise NoDetails(prefix)
+        if os.path.isdir(path):
+            raise InvalidDetailsFile(prefix)
+        
+        f = open(path)
+        text = f.read()
+        f.close()
+        
+        text = re.sub("(?m)^#.*\n", "", text)
+        
+        while True:
+            oldtext = text
+            retext = re.sub("\[\w+\]\s+\[", "[", text)
+            text = retext
+            if oldtext == retext:
+                break
+        
+        text = re.sub("\[\w+\]\s*$", "", text)
+        
+        print(text.strip())
     
-    def edit_bug(self, prefix):
+    def edit(self, prefix, editor='notepad'):
         """Allows the user to edit the details of the specified bug"""
         pass
     
-    def comment_bug(self, prefix, comment):
+    def comment(self, prefix, comment):
         """Allows the user to add a comment to the bug without launching an editor"""
         pass
     
-    def resolve_bug(self, prefix):
+    def resolve(self, prefix):
         """Marks a bug as resolved"""
         task = self[prefix]
         task['open'] = 'False'
     
-    def reopen_bug(self, prefix):
+    def reopen(self, prefix):
         """Reopens a bug that was previously resolved"""
         task = self[prefix]
         task['open'] = 'True'
     
-    def list_bugs(self,open=True,owner='*',grep='',verbose=False,quiet=False):
+    def list(self,open=True,owner='*',grep='',verbose=False,quiet=False):
         """Lists all bugs, applying the given filters"""
         tasks = dict(self.bugs.items())
         label = 'prefix' if not verbose else 'id'
@@ -250,20 +324,17 @@ class BugsDict(object):
                 tasks[task_id]['prefix'] = prefix
         
         plen = max(map(lambda t: len(t[label]), tasks.values())) if tasks else 0
+        printed = 0
         for task in tasks.values():
             if (open and task['open'] == 'True') or (not open and task['open'] != 'True'):
                 if owner == '*' or owner == task['owner']:
                     if grep.lower() in task['text'].lower():
+                        printed += 1
                         p = '%s - ' % task[label].ljust(plen) if not quiet else ''
                         print(p + task['text'])
+        print(_describe_print(printed,open,owner,grep))
+
 #
 # Mercurial Extention Methods
 # These are used to allow the tool to work as a Hg Extention
-#
-
-
-#
-# Mercurial Replacement Methods
-# These are used to allow this tool to work without being a Hg Extension
-# And are not called when run as part of Hg
 #
